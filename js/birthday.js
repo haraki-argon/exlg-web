1791ec78b4c557b34a22c67d3247dff022e2279a
haraki <2150864207@qq.com>
2021-08-10 15:57:08 +0800

    re 0.2.2.1 fix birthday bug

--- js/birthday.js
+++ js/birthday.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var birth =[[0,0],[5,7],[8,17],[0,0],[0,0],[12,6],[8,10],[11,10],[0,0],[1,2],[4,21],[1,16],[8,10]]
+	var birth =[[0,0],[5,7],[8,17],[0,0],[0,0],[0,0],[12,6],[8,10],[11,10],[0,0],[0,0],[0,0],[1,2],[4,21],[1,16],[8,10]]
 	var da = new Date();
 	var nmon = da.getMonth();
 	var nday = da.getDate();
@@ -11,8 +11,8 @@ $(document).ready(function(){
 	for(var i=1;i<birth.length;i++){
 		console.log(birth[i][0]+" "+birth[i][1]);
 		if(nmon+1==birth[i][0]&&nday==birth[i][1]){
-			tep[i].className="cheer";
-			ter[i].title="祝 "+ten[i].innerText+" 生日快乐！";
+			tep[i-1].className="cheer";
+			ter[i-1].title="祝 "+ten[i-1].innerText+" 生日快乐！";
 		}
 	}
 });
